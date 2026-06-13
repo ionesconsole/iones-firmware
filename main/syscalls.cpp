b46d67e203dd346233095478f12d1b5ee16507f8
abes400 <125301699+abes400@users.noreply.github.com>
2026-06-13 08:43:36 +0300

    ADD basic syscalls

--- main/syscalls.cpp
+++ main/syscalls.cpp
@@ -4,6 +4,9 @@
 
 void init_syscalls(WRState* wr_state) {
     wr_registerFunction(wr_state, "print", _sys_print);
+	wr_registerFunction(wr_state, "up_press", _sys_up_press_test);
+	wr_registerFunction(wr_state, "dn_press", _sys_dn_press_test);
+	wr_registerFunction(wr_state, "a_press", _sys_a_press_test);
 
 
     // TODO: bind all syscalls here.
@@ -16,4 +19,20 @@ void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& ret
 	{
 		tft.printf( "%s", argv[i].asString(buf,1024) );
 	}
+}
+
+
+bool up_press();
+bool dn_press();
+bool a_press();
+
+void _sys_up_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+	retVal = up_press();
+}
+void _sys_dn_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+	retVal = dn_press();
+}
+
+void _sys_a_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+	retVal = a_press();
 }
--- main/syscalls.h
+++ main/syscalls.h
@@ -5,3 +5,6 @@
 
 void init_syscalls(WRState* wr_state);
 void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+void _sys_up_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+void _sys_dn_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+void _sys_a_press_test( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
