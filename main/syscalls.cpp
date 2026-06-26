4ba2a2e7a8e477f9d98f042e929e20407f18898a
abes400 <125301699+abes400@users.noreply.github.com>
2026-06-27 02:38:42 +0300

    ADD game button syscalls

--- main/syscalls.cpp
+++ main/syscalls.cpp
@@ -1,19 +1,44 @@
 #include "syscalls.h"
 #include "globals.h"
+#include "input.h"
 
 
 void init_syscalls(WRState* wr_state) {
-    wr_registerFunction(wr_state, "print", _sys_print);
-
-
-    // TODO: bind all syscalls here.
+	// TODO: bind all syscalls here.
+    wr_registerFunction(wr_state, "print", 			_sys_print			);
+	wr_registerFunction(wr_state, "printOut", 		_sys_printout		);
+	wr_registerFunction(wr_state, "readInput", 		_sys_read_gm_input	);
+	wr_registerFunction(wr_state, "isKeyDown", 		_sys_key_down		);
+	wr_registerFunction(wr_state, "isKeyPressed", 	_sys_key_pressed	);
+	wr_registerFunction(wr_state, "isKeyReleased", 	_sys_key_released	);
 }
 
-void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
-{
+
+
+// TODO: implement all syscalls here.
+
+void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
 	char buf[256];
-	for( int i=0; i<argn; ++i )
-	{
+	for( int i=0; i<argn; ++i ) {
 		tft.printf( "%s", argv[i].asString(buf,1024) );
 	}
 }
+
+void _sys_printout( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+	char buf[256];
+	for( int i=0; i<argn; ++i ) {
+		printf( "%s", argv[i].asString(buf,1024) );
+	}
+}
+
+void _sys_read_gm_input ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ read_gm_input(); }
+
+void _sys_key_down ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ wr_makeInt(&retVal, gm_inputs & (uint8_t) argv -> asInt()); }
+
+void _sys_key_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ wr_makeInt(&retVal, gm_inputs & (uint8_t) argv -> asInt() && !(last_gm_inputs & (uint8_t) argv -> asInt())); }
+
+void _sys_key_released ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ wr_makeInt(&retVal, !(gm_inputs & (uint8_t) argv -> asInt()) && last_gm_inputs & (uint8_t) argv -> asInt()); }
--- main/syscalls.h
+++ main/syscalls.h
@@ -2,15 +2,44 @@
 
 #include "wrench.h"
 
-
 /**
  * Binds the syscalls to the wrench script.
  */
 void init_syscalls(WRState* wr_state);
 
+
+// TODO: Declare all syscalls here.
+
 /**
- * Prints message to the screen
+ * Prints message to the screen.
  */
 void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
 
-// TODO: Declare all syscalls here.
+
+/**
+ * Prints message to idf monitor.
+ */
+void _sys_printout( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+
+/**
+ * Reads the current state of the buttons.
+ */
+void _sys_read_gm_input( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+
+/**
+ * Checks if the key given is down.
+ */
+void _sys_key_down ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+
+/**
+ * Checks if the key given is pressed.
+ */
+void _sys_key_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+/**
+ * Checks if the key given is released.
+ */
+void _sys_key_released ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
