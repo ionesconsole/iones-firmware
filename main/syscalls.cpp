60bda5396a69745157c206f7d4b4fb00900d31c9
abes400 <125301699+abes400@users.noreply.github.com>
2026-06-27 04:04:58 +0300

    ADD select start check syscalls

--- main/syscalls.cpp
+++ main/syscalls.cpp
@@ -5,26 +5,34 @@
 
 void init_syscalls(WRState* wr_state) {
 	// TODO: bind all syscalls here.
+	wr_registerFunction(wr_state, "clearTerminal", 	_sys_clear_scr		);
     wr_registerFunction(wr_state, "print", 			_sys_print			);
 	wr_registerFunction(wr_state, "printOut", 		_sys_printout		);
 	wr_registerFunction(wr_state, "readInput", 		_sys_read_gm_input	);
 	wr_registerFunction(wr_state, "isKeyDown", 		_sys_key_down		);
 	wr_registerFunction(wr_state, "isKeyPressed", 	_sys_key_pressed	);
 	wr_registerFunction(wr_state, "isKeyReleased", 	_sys_key_released	);
+	wr_registerFunction(wr_state, "selectPressed", 	_sys_select_pressed	);
+	wr_registerFunction(wr_state, "startPressed", 	_sys_start_pressed	);
 }
 
 
 
 // TODO: implement all syscalls here.
 
-void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+void _sys_clear_scr ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+	tft.fillRect(0, 0, 320, 240, TFT_BLACK);
+	tft.setCursor(0, 0);
+}
+
+void _sys_print ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
 	char buf[256];
 	for( int i=0; i<argn; ++i ) {
 		tft.printf( "%s", argv[i].asString(buf,1024) );
 	}
 }
 
-void _sys_printout( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
+void _sys_printout ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr ) {
 	char buf[256];
 	for( int i=0; i<argn; ++i ) {
 		printf( "%s", argv[i].asString(buf,1024) );
@@ -42,3 +50,9 @@ void _sys_key_pressed ( WRContext* c, const WRValue* argv, const int argn, WRVal
 
 void _sys_key_released ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
 { wr_makeInt(&retVal, !(gm_inputs & (uint8_t) argv -> asInt()) && last_gm_inputs & (uint8_t) argv -> asInt()); }
+
+void _sys_select_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ wr_makeInt(&retVal, select_pressed()); }
+
+void _sys_start_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr )
+{ wr_makeInt(&retVal, start_pressed()); }
--- main/syscalls.h
+++ main/syscalls.h
@@ -11,28 +11,29 @@ void init_syscalls(WRState* wr_state);
 // TODO: Declare all syscalls here.
 
 /**
+ * Clears screen terminal (Warning, the screen will go black when called);
+ */
+void _sys_clear_scr ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+/**
  * Prints message to the screen.
  */
-void _sys_print( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
-
+void _sys_print ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
 
 /**
  * Prints message to idf monitor.
  */
-void _sys_printout( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
-
+void _sys_printout ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
 
 /**
  * Reads the current state of the buttons.
  */
-void _sys_read_gm_input( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
-
+void _sys_read_gm_input ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
 
 /**
  * Checks if the key given is down.
  */
 void _sys_key_down ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
-
 
 /**
  * Checks if the key given is pressed.
@@ -43,3 +44,13 @@ void _sys_key_pressed ( WRContext* c, const WRValue* argv, const int argn, WRVal
  * Checks if the key given is released.
  */
 void _sys_key_released ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+/**
+ * Checks if Select was pressed.
+ */
+void _sys_select_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
+
+/**
+ * Checks if Start was pressed.
+ */
+void _sys_start_pressed ( WRContext* c, const WRValue* argv, const int argn, WRValue& retVal, void* usr );
