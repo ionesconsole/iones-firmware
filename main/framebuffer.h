818da18762bd176f66a7cd3d76e9966169c4586e
RodgerSmither <onuremremisirli@gmail.com>
2026-07-02 01:42:01 +0300

    Implement framebuffer updates

--- main/framebuffer.h
+++ main/framebuffer.h
@@ -15,18 +15,55 @@
 #define RGB565_MAGENTA 0xF81F
 #define RGB565_CYAN 0x07FF
 
-bool fb_init();
+
+#define FB_DEPTH_BUFFER   0x01
+#define FB_STENCIL_BUFFER 0x02
+
+
+struct DirtyRect{
+
+
+};
+
+
+struct FrameBuffer{
+    int width;
+    int height;
+    uint16_t* colorBuffer;   
+    uint16_t* depthBuffer;   // optional depth buffer
+    uint8_t*  stencilBuffer; // optional stencil buffer
+    bool hasColor;
+    bool hasDepth;
+    bool hasStencil;
+    FrameBuffer()
+        : width(TFT_WIDTH),
+          height(TFT_HEIGHT),
+          colorBuffer(nullptr),
+          depthBuffer(nullptr),
+          stencilBuffer(nullptr),
+          hasColor(false),
+          hasDepth(false),
+          hasStencil(false)
+    {
+    }
+};
+
+
+
+
+bool fb_init(uint8_t flags = 0);
 
 void fb_begin_frame(uint16_t color);
 void fb_end_frame();
 
 void fb_clear(uint16_t color);
-void fb_set_pix(int x, int y, uint16_t color);
+void fb_set_pixel(int x, int y, uint16_t color);
+void fb_get_pixel();
 
 //drawing functions
 
 void fb_drawRectangle(int x, int y, int height, int weight, uint16_t color);
 
 //flush to tft
-
+//void fb_push_wrapper();
 void fb_push_tft();
